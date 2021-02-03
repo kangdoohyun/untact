8e3c74d375f59bb7d473c25ed30df95c31816040
kangdoohyun <rkdengus1208@gmail.com>
2021-02-03 23:58:52 +0900

    01 31

--- src/main/java/com/sbs/untact/controller/UsrArticleController.java
+++ src/main/java/com/sbs/untact/controller/UsrArticleController.java
@@ -1,85 +1,81 @@
 package com.sbs.untact.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sbs.untact.dto.Article;
 import com.sbs.untact.dto.ResultData;
-import com.sbs.untact.util.Util;
+import com.sbs.untact.service.ArticleService;
 
 @Controller
 public class UsrArticleController {
-	List<Article> articles;
-	private int articlesLastId;
+	@Autowired
+	private ArticleService articleService;
 	
-	public UsrArticleController() {
-		articlesLastId = 0;
-		articles = new ArrayList<>();
-		
-		articles.add(new Article(++articlesLastId, "2020/01/28", "2020/01/28", "제목", "내용"));
-		
-	}
 	@RequestMapping("/usr/article/list")
 	@ResponseBody
-	public List<Article> showList(){
-		return articles;
+	public List<Article> showList(String searchKeywordType, String searchKeyword){
+		if(searchKeywordType != null) {
+			searchKeywordType = searchKeywordType.trim();
+		}
+		if(searchKeywordType == null || searchKeywordType.length() == 0) {
+			searchKeywordType = "titleAndBody";
+		}
+		if(searchKeyword != null && searchKeyword.length() == 0) {
+			searchKeyword = null;
+		}
+		if(searchKeyword != null) {
+			searchKeyword = searchKeyword.trim();
+		}
+		return articleService.getArticles(searchKeywordType, searchKeyword);
 	}
 	@RequestMapping("/usr/article/detail")
 	@ResponseBody
 	public Article showDetail(int id) {
-		return articles.get(id - 1);
+		Article article = articleService.getArticle(id);
+		return article;
 	}
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
-	public ResultData doAdd(String title, String body) {
-		String regDate = Util.getNowDate();
-		String updateDate = regDate;
+	public ResultData doAdd(@RequestParam Map<String, Object> param) {
+		if (param.get("title") == null) {
+			return new ResultData("F-1", "title을 입력해주세요.");
+		}
+
+		if (param.get("title") == null) {
+			return new ResultData("F-1", "body를 입력해주세요.");
+		}
 		
-		articles.add(new Article(++articlesLastId, regDate, updateDate, title, body));
-		return new ResultData("S-1", "성공하였습니다.", "id", articlesLastId);
+		return articleService.addArticle(param);
 	}
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public ResultData doDelete(int id) {
-		boolean Select = deleteArticle(id);
-		if(Select == false) {
+		Article article = articleService.getArticle(id);
+		if(article == null) {
 			return new ResultData("F-1", "존재하지 않는 게시물입니다");
-			
 		}
-		return new ResultData("S-1", "게시물을 삭제했습니다", "id", id);
-	}
-	private boolean deleteArticle(int id) {
-		for(Article article : articles) {
-			if(article.getId() == id) {
-				articles.remove(article);
-				return true;
-			}
-		}
-		return false;
+		
+		return articleService.deleteArticle(id);
 	}
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public ResultData doModify(int id, String title, String body) {
-		Article selArticle = null;
-		
-		for (Article article : articles) {
-			if(article.getId() == id) {
-				selArticle = article;
-				break;
-			}
+	public ResultData doModify(Integer id, String title, String body) {
+		if(id == null) {
+			return new ResultData("F-1", "id를 입력해주세요");
 		}
-		if(selArticle == null) {
-			return new ResultData("F-1", String.format("%d번 게시물은 존재하지 않습니다", id));
+		if(title == null) {
+			return new ResultData("F-1", "title을 입력해주세요");
 		}
-		selArticle.setUpdateDate(Util.getNowDate());
-		selArticle.setTitle(title);
-		selArticle.setBody(body);
-		
-		return new ResultData("S-1", String.format("%d번 게시물이 수정되었습니다", id), "id", id);
+		if(body == null) {
+			return new ResultData("F-1", "body를 입력해주세요");
+		}
+		return articleService.modifyArticle(id, title, body);
 	}
 }
--- src/main/java/com/sbs/untact/util/Util.java
+++ src/main/java/com/sbs/untact/util/Util.java
@@ -1,7 +1,9 @@
 package com.sbs.untact.util;
 
+import java.math.BigInteger;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Util {
@@ -15,25 +17,49 @@ public class Util {
 		return regDate;
 	}
 	public static Map<String, Object> mapOf(Object...args){
-		int size = args.length / 2;
 		if(args.length % 2 != 0) {
 			throw new IllegalArgumentException("인자를 짝수개 입력해주세요");
 		}
+		int size = args.length / 2;
+		
+		Map<String, Object> map = new LinkedHashMap<>();
+		
 		for(int i = 0; i < size; i++) {
 			int keyIndex =  i * 2;
 			int valueIndex = keyIndex + 1;
+			
 			String key;
 			Object value;
+			
 			try {
 				key = (String)args[keyIndex];
 			}
 			catch (ClassCastException e){
 				throw new IllegalArgumentException("키는 문자로 입력해주세요" + e.getMessage());	
 			}
-			
+			value = args[valueIndex];
+
+			map.put(key, value);
 		}
 		
-		return null;
+		return map;
+	}
+	public static int getAsInt(Object object, int defaultValue) {
+		if (object instanceof BigInteger) {
+			return ((BigInteger) object).intValue();
+		} else if (object instanceof Double) {
+			return (int) Math.floor((double) object);
+		} else if (object instanceof Float) {
+			return (int) Math.floor((float) object);
+		} else if (object instanceof Long) {
+			return (int) object;
+		} else if (object instanceof Integer) {
+			return (int) object;
+		} else if (object instanceof String) {
+			return Integer.parseInt((String) object);
+		}
+
+		return defaultValue;
 	}
 	
 }
